cbba39ba35c2b0a1241733cf759c457138f99bf7
Cem Olcay <c-cem.olcay@charter.com>
2019-09-18 11:25:18 +0300

    Initial Commit

---
+++ JSONTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONTableView
+//
+//  Created by cem.olcay on 18/09/2019.
+//  Copyright © 2019 cemolcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
